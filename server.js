ac6a68b229586164f3dacbdfb08ea5c6e45ec4b4
SuryaJanardhanGS <surya@growstack.biz>
2025-08-06 10:01:55 +0530

    Day 1 : server setup post , get, (req,res), postman

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const axios = require("axios");
+
+const app = express();
+// app == server
+const port = 5000;
+
+app.use(express.json());
+
+// server -- POST GET PUT DELETE
+// req.body = userdata;
+app.post("/verify-user", async (req, res) => {
+  console.log(req.body);
+  const data = req.body;
+    
+  console.log(data);
+
+  //   const { name, password } = userdata;
+
+  res.status(200).json({ message: "valid user", userdata: data.userdata });
+});
+
+app.listen(port, () => {
+  console.log("server running on port 5000");
+});
