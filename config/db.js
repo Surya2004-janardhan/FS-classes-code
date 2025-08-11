c6810f17d626a277d2e833445e8b59f918f5d135
Surya Janardhan <chintalajanardhan2004@gmail.com>
2025-08-11 09:02:03 +0530

    completed CRUD with db

---
+++ config/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const connectToDb = async () => {
+  await mongoose.connect("uri");
+  console.log("connected to our mongoDB");
+};
+
+module.exports = connectToDb;
