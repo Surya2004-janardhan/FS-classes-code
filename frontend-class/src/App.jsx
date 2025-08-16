162d4f8cb8929efbb08c3a030787868e7862a167
Surya Janardhan <chintalajanardhan2004@gmail.com>
2025-08-16 22:43:35 +0530

    useeffect

--- frontend-class/src/App.jsx
+++ frontend-class/src/App.jsx
@@ -4,11 +4,39 @@ import Body from "./components/Body";
 import Footer from "./components/Footer";
 import Test from "./components/Test";
 import Child from "./components/props";
+
+import { useEffect, useState } from "react";
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const data = [];
+  useEffect(() => {
+
+    console.log("useEffect called");
+  }, [count ]);
+
+  const handlechange = () => {
+    setCount(count + 1);
+  };
+
   return (
     <>
-      <Child name="janardhan" />
-      
+    {data &&
+      <h1>{data}</h1>}
+          
+      <div>
+        <p>{count}</p>
+        <button
+          onClick={() => {
+            handlechange();
+          }}
+        >
+          click here{" "}
+        </button>
+        <Child name="janardhan" />
+      </div>
+  
+
     </>
   );
 }
